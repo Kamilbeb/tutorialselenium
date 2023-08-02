3e87ee780cd090db2459680cf51fcd9b338015ac
kamilbeb <bebkampoczta@gmail.com>
2023-08-02 13:00:50 +0200

    swich to iframe

--- src/test/java/iframeTest.java
+++ src/test/java/iframeTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Set;
@@ -12,8 +13,10 @@ public class iframeTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
+        WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));  //inny sposób przełączania między oknami
+        driver.switchTo().frame(iframe);
         driver.manage().window().maximize();
-        driver.switchTo().frame(0);  //przełączenie na ifrema
+        //driver.switchTo().frame(0);  //przełączenie na ifrema
         driver.findElement(By.id("fname")).sendKeys("Kamil");
         driver.switchTo().defaultContent(); //powrót do pierwotnej strony
         System.out.println(driver.findElement(By.tagName("h1")).getText());
