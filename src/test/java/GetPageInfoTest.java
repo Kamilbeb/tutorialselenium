7d607b17bdd8ec95c9f024b789e430a3469f4f12
kamilbeb <bebkampoczta@gmail.com>
2023-08-03 15:30:26 +0200

    get title and Url

---
+++ src/test/java/GetPageInfoTest.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetPageInfoTest {
+    @Test
+    public void getInfo(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+
+    }
+}
