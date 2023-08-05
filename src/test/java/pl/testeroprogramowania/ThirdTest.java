9a3f088c673e07e572418ac35f57e62b60094fca
kamilbeb <bebkampoczta@gmail.com>
2023-08-05 15:12:25 +0200

    TestNG

---
+++ src/test/java/pl/testeroprogramowania/ThirdTest.java
@@ -0,0 +1,19 @@
+package pl.testeroprogramowania;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest {
+
+    @Test(priority = 0)
+    public void firstTest(){
+        System.out.println("I am first test");
+    }
+    @Test(priority = 1)
+    public void secondTest(){
+        System.out.println("I am second test");
+    }
+    @Test(priority = 2)
+    public void thirdTest(){
+        System.out.println("I am third test");
+    }
+}
