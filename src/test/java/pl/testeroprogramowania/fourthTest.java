92e10af24b7a70ca5cac6f66edc2cc391149b2c5
kamilbeb <bebkampoczta@gmail.com>
2023-08-07 11:54:04 +0200

    add class dataProvider

--- src/test/java/pl/testeroprogramowania/fourthTest.java
+++ src/test/java/pl/testeroprogramowania/fourthTest.java
@@ -1,5 +1,6 @@
 package pl.testeroprogramowania;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class fourthTest {
@@ -15,5 +16,19 @@ public class fourthTest {
     @Test
     public void thirdTest(){
         System.out.println("I am third test");
+    }
+
+
+    @Test(dataProvider = "data")        //określamy że nasza metoda bedzie korzystała z dataProvider
+    public void dpTest(String val, String number){
+
+        System.out.println(val);
+        System.out.println(number);
+
+    }
+
+    @DataProvider(name = "data")        //oznaczamy dataProvidera 0raz w nawiasie tworzymy jego nazwę
+    public Object[][] dataProvider(){       //będzie zwracało tablicę dwuwymiarową
+        return new Object[][]{{"I am first test","First"},{"I am second test","Second"},{"I am third test","Third"}}; //nasza metoda będzie zwracała 3 wartości podane w klamrach
     }
 }
