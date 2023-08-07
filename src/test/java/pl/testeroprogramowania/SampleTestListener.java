6cf74368db749a5b1e59f9905276a642f60c307e
kamilbeb <bebkampoczta@gmail.com>
2023-08-07 11:52:58 +0200

    add screenshot

--- src/test/java/pl/testeroprogramowania/SampleTestListener.java
+++ src/test/java/pl/testeroprogramowania/SampleTestListener.java
@@ -1,8 +1,17 @@
 package pl.testeroprogramowania;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SampleTestListener implements ITestListener{
 
@@ -21,7 +30,18 @@ public class SampleTestListener implements ITestListener{
     @Override
     public void onTestFailure(ITestResult result) {
         ITestListener.super.onTestFailure(result);
-        System.out.println("I am taking screenshot");
+        WebDriver driver = DriverFactory.getDriver();
+        int randomNumber = (int) (Math.random()*1000);
+
+        TakesScreenshot screenshot = (TakesScreenshot) driver; //castowanie drivera
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        String fileName = "faildTest"+randomNumber+".png";
+
+        try {
+            FileUtils.copyFile(before,new File("src/test/resources/"+fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
